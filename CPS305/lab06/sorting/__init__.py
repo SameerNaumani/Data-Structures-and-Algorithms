b27e075a09112c004d38cfc0667d36800340354d
Sameer Naumani <snaumani97@gmail.com>
2019-10-27 22:04:41 -0400

    lab 6

--- CPS305/lab06/sorting/__init__.py
+++ CPS305/lab06/sorting/__init__.py
@@ -2,7 +2,7 @@
 # for selecting a pivot value as amodification to quickSort (ame this function mo3_quickSort). 
 # Prepare test cases for your mo3_quickSort
 
-def quickSort(alist):
+def mo3_quickSort(alist):
     quickSortHelper(alist,0,len(alist)-1)
 
 def quickSortHelper(alist,first,last):
@@ -15,7 +15,7 @@ def quickSortHelper(alist,first,last):
 def partition(alist,first,last):
     #find middle index
     middleindex = median(alist,first,last,(first+last)//2)
-    print(middleindex)
+    #print(middleindex)
     #swap first element with the pivot value chosen
     alist[first],alist[middleindex] = alist[middleindex],alist[first]
     
@@ -51,4 +51,46 @@ def median(alist, first, last, middle):
         return last if alist[last] < alist[middle] else int(middle)
     else:
         return first if alist[first] < alist[middle] else int(middle)
+    
+    
+    
+#Original Quick Sort
+def quickSort(alist):
+    quickSortHelper2(alist,0,len(alist)-1)
+
+def quickSortHelper2(alist,first,last):
+    if first<last:
+        splitpoint = partition2(alist,first,last)
+        quickSortHelper2(alist,first,splitpoint-1)
+        quickSortHelper2(alist,splitpoint+1,last)
+
+
+def partition2(alist,first,last):
+    pivotvalue = alist[first]
+
+    leftmark = first+1
+    rightmark = last
+
+    done = False
+    while not done:
+
+        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+
+        while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark -1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+
+
+    return rightmark
 
--- CPS305/lab06/test.py
+++ CPS305/lab06/test.py
@@ -1,107 +1,31 @@
 import unittest
-from hashing import HashTable
+from sorting import mo3_quickSort
 
 class TestGenerate(unittest.TestCase):
-    #Create test cases that test putting key-data pairs in the table before table resizing, and then getting that data after tableresizing.
-    
-    #Test if the size remains 11 as long as the max threshold is not reached by putting in elements
+   
+    #test to see if size is generated correctly
     def test_size(self):
-        H = HashTable()
-        H[54]="cat"
-        H[26]="dog"
-        H[93]="lion"
-        H[17]="tiger"
-        H[77]="bird"
-        H[31]="cow"
+        alist = [54,26,93,17,77,31,44,55,20]
+        mo3_quickSort(alist)
+        result = len(alist)
         
-        self.assertEqual(H.size, 11)
+        self.assertEqual(result, 9)
    
 
-    #Test if the size actually doubles when 80% of max threshold is reached by putting in elements
-    def test_growth(self):
-        H = HashTable()
-        H[54]="cat"
-        H[26]="dog"
-        H[93]="lion"
-        H[17]="tiger"
-        H[77]="bird"
-        H[31]="cow"
-        H[44]="goat"
-        H[55]="whale" #8 
-        H[20]="chicken" #9
-     
-        self.assertEqual(H.size, 23)
-    
-    #Test getting the data before resize
-    def test_getData(self):
-        H = HashTable()
-        H[54]="cat"
-        H[26]="dog"
-        H[77]="bird"
-        H[31]="cow"
-        H[44]="goat"
-        H[55]="whale" #8 
-        H[20]="chicken" #9
+    #Test to see if sorting is done correctly
+    def test_sort(self):
+        alist = [54,26,93,17,77,31,44,55,20]
+        mo3_quickSort(alist)
+        result = alist
+        self.assertEqual(result, [17, 20, 26, 31, 44, 54, 55, 77, 93])
         
-        self.assertEqual(H[20], 'chicken')
-    
-    #Test getting the data after resize 
-    def test_getData2(self):
-        H = HashTable()
-        H[54]="cat"
-        H[26]="dog"
-        H[93]="lion"
-        H[17]="tiger"
-        H[77]="bird"
-        H[31]="cow"
-        H[44]="goat"
-        H[55]="whale" #8 
-        H[20]="chicken" #9
+    def test_sort3(self):
+        alist = [22,33,54,26,93,17,77,31,44,55,20]
+        mo3_quickSort(alist)
+        result = alist
+        self.assertEqual(result, [17, 20,22, 26, 31, 33, 44, 54, 55, 77, 93])
         
-        self.assertEqual(H[20], 'chicken')
         
-    def test_getData3(self):
-        H = HashTable()
-        H[54]="cat"
-        H[26]="dog"
-        H[93]="lion"
-        H[17]="tiger"
-        H[77]="bird"
-        H[31]="cow"
-        H[44]="goat"
-        H[55]="salamander"
-        H[20]="chicken"
-        H[21]= "whale"    #10
-        H[34] = "chihuahua" #11
-        H[45] = "mammoth" #12 
-        
-        self.assertEqual(H.slots[2], None)
-        
-    #Check to see if it resizes to a prime number for the third resize       
-    def test_getData4(self):
-        H = HashTable()
-        H[54]="cat"
-        H[26]="dog"
-        H[93]="lion"
-        H[17]="tiger"
-        H[77]="bird"
-        H[31]="cow"
-        H[44]="goat"
-        H[55]="salamander"
-        H[20]="chicken"
-        H[21]= "whale"    #10
-        H[34] = "chihuahua" #11
-        H[45] = "mammoth" #12 
-        H[72] = "charizard"
-        H[33] = "pikachu"
-        H[56] = "raptor"
-        H[79] = "BlueJay"
-        H[99] = "Dragon" #17
-        H[61] = "Bat" #18
-        H[68] = "Batman" #19
-         
-        self.assertEqual(H.size, 47)
-
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
